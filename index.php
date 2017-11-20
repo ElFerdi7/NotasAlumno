992a603bd788d11d30cc79cc8e2cc56538e62eff
Fernando <Fernando@192.168.1.70>
2017-11-20 21:25:22 +0100

    fgh

--- index.php
+++ index.php
@@ -27,16 +27,62 @@ and open the template in the editor.
 
         echo "<strong>Ejercicio b</strong>";
         echo"<br>";
+
         function mostrarNotasAlumnos($i) {
             foreach ($i as $ind1 => $valor1) {
                 foreach ($valor1 as $ind2 => $valorReal) {
                     echo "El alumno $ind1 de la asignatura $ind2 ha obtenido un $valorReal<br/>";
-                }; 
-            };          
-        }; 
-        
-        echo mostrarNotasAlumnos ($notasAlumnos);
+                }
+            }
+        }
+
+        echo mostrarNotasAlumnos($notasAlumnos);
         echo"<br>";
+        echo"<br>";
+
+
+        echo "<strong>Ejercicio c</strong>";
+        echo"<br>";
+        $mostrarAlumno = "AL1";
+
+        function mostrarNotasAlumno($alumno, $array) {
+            foreach ($array as $key2 => $value2) {
+                if ($key2 == $alumno) {
+                    foreach ($value2 as $key3 => $value3) {
+                        echo "El alumno " . $key2 . " de la asignatura " . $key3 . " ha obtenido un " . $value3 . "<br>";
+                    }
+                }
+            }
+        }
+
+        echo mostrarNotasAlumno($mostrarAlumno, $notasAlumnos);
+        echo "<br>";
+        echo "<br>";
+        echo "<strong>Ejercicio d</strong>";
+        echo "<br>";
+
+        $mostrarAlumno2 = "AL2";
+
+        function mostrar_NotaMasAlta_Alumno($alumno2, $arrayNotas) {
+            foreach ($arrayNotas as $clave => $valor) {
+                if ($clave == $alumno2) {
+                    list(, $valor1) = each($valor);
+                    $max = $valor1;
+                    foreach ($valor as $clave1 => $valor2) {
+                        if ($max < $valor2) {
+                            $max = $valor2;
+                            $asig = $clave1;
+                        } else {
+                            $max = $max;
+                            $asig = $clave1;
+                        }
+                    }
+                }
+            }
+            return "La nota mas alta obtenida por " . $alumno2 . " ha sido en " . $asig . " con un " . $max;
+        }
+
+        echo mostrar_NotaMasAlta_Alumno($mostrarAlumno2, $notasAlumnos);
         ?>
     </body>
 </html>
